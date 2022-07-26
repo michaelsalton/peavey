e560dc3b7bb6c2d637f8a8b348da250dbc39be57
Michael Salton <58754252+michaelsalton@users.noreply.github.com>
2022-07-25 20:32:47 -0400

    Add files via upload

---
+++ BinList.swift
@@ -0,0 +1,61 @@
+//
+//  BinData.swift
+//  PeaveyIndustries
+//
+//  Created by Michael Salton on 2022-07-23.
+//
+
+import Foundation
+
+public var binList = [
+    
+    "AP2",
+    "AP3",
+    "AP4",
+    "AP8",
+    "BP2",
+    "BP3",
+    "BP4",
+    "CP3",
+    "CP4",
+    "TY1",
+    "TY2",
+    "TY3",
+    "TY4",
+    "FA",
+    "FB",
+    "FC",
+    "FD",
+    "FE",
+    "FF",
+    "FG",
+    "FH",
+    "RA",
+    "RB",
+    "RC",
+    "RD",
+    "RE",
+    "RG",
+    "TL",
+    "TK",
+    "TM",
+    "TJ",
+    "TG1",
+    "TG2",
+    "TG3",
+    "TG4",
+    "EA",
+    "EB",
+    "EC",
+    "ED",
+    "EE",
+    "EG",
+    "EH",
+    "EI",
+    "E4",
+    "C4",
+    "CK",
+    "WR",
+    "DL",
+    "YA",
+]
