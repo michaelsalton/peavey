e3d943203f0f61d8bdfbce95dc5eda811889eed4
Michael Salton <58754252+michaelsalton@users.noreply.github.com>
2022-07-26 19:20:24 -0400

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -7,19 +7,6 @@
 
 import SwiftUI
 
-struct ImageOverlay: View {
-    
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.green)
-                .frame(width: 3, height: 132)
-                .position(x: 332, y: 184)
-        }
-            
-    }
-}
-
 struct ContentView: View {
     
     @State private var isZoomed = false
@@ -27,16 +14,35 @@ struct ContentView: View {
     @State var searchText = ""
     
     var body: some View {
-        VStack {
-            Image("map")
-                .resizable()
-                .scaledToFit()
-                .position(x: 195, y: 175)
-                .overlay(ImageOverlay(), alignment: .bottomTrailing)
-            Spacer()
-        }
         
+        // SEARCH FOR BIN
         NavigationView {
+            
+            // MAP
+            VStack {
+                Image("map")
+                    .resizable()
+                    .scaledToFit()
+                    .position(x: 195, y: 175)
+                    .overlay(ImageOverlay(), alignment: .bottomTrailing)
+                Spacer()
+            }
+            
+            struct ImageOverlay: View{
+                // DRAW BIN LOCATION FUNCTION
+                func drawBox(width: CGFloat, height: CGFloat, x: CGFloat, y: CGFloat) -> ZStack<Rectangle> {
+                        var output: ZStack<Rectangle> {
+                            ZStack {
+                            Rectangle()
+                                .fill(.red)
+                                .frame(width: width, height: height)
+                                .position(x: x, y: y) as! Rectangle
+                            }
+                    }
+                    return output
+                }
+            }
+            
             List {
                 ForEach(bins, id: \.self) { bin in
                     HStack {
@@ -48,32 +54,27 @@ struct ContentView: View {
                     }
                     .padding()
                 }
-                
             }
             .searchable(text: $searchText)
             .navigationTitle("Bins")
+            
+            // CALULATE BOX SIZE AND LOCATION
+            if searchText.elementsEqual("AP1") {
+                drawBox(width: 50, height: 50, x: 50, y: 50)
+            }
         }
-        
-        
-        
     }
-    
-
-    
-    
-    
+            
     var bins: [String] {
         let upBins = listOfBins.map {$0.uppercased()}
-        
         return searchText == "" ? upBins : upBins.filter{
             $0.contains(searchText.uppercased())
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
-
