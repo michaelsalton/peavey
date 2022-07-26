c99c84e8db91f586be7b128cb904b6d117aa2d8b
Michael Salton <58754252+michaelsalton@users.noreply.github.com>
2022-07-25 20:32:28 -0400

    Add files via upload

---
+++ PeaveyMap.swift
@@ -0,0 +1,17 @@
+//
+//  PeaveyIndustriesApp.swift
+//  PeaveyIndustries
+//
+//  Created by Michael Salton on 2022-07-23.
+//
+
+import SwiftUI
+
+@main
+struct PeaveyMap: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
